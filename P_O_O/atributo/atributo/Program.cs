4ed9f27c434b323428bca8bfa6a5e1f7635851bc
camilo batero <cbateroc@gmail.com>
2020-03-02 14:45:40 -0500

    guardando ejercicio

---
+++ P_O_O/atributo/atributo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        aprendiz apSena = new aprendiz();
+        apSena.Nombre = "camilo humberto ";
+        apSena.apellidos = "batero cuellar";
+        apSena.DepartamentodeResidencia = " Huila ";
+        apSena.Municipioderesidencia = "Pitalito ";
+        apSena.direccion = "carrera 5 # 11-53 ";
+        apSena.genero = "masculino"
+        saludo(apSena);
+
+    }
+    private static void saludo(aprendiz apSena)
+    {
+        Console.WriteLine("Bienvenido,sr(a)" + apSena.Nombre + apSena.apellidos );
+        Console.WriteLine("tu ciudad es: " + apSena.DepartamentodeResidencia);
+        Console.WriteLine("tu municipio es: " + apSena.Municipioderesidencia);
+        Console.WriteLine("tu dirección es: " + apSena.direccion);
+        Console.ReadLine();
+    }
+}
